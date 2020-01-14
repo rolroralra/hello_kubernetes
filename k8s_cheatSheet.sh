92e68fec9264054d5fe0775decd290a7e60534b5
rolroralra <rolroralra@gmail.com>
2020-01-14 18:36:28 +0900

    NodePort, LoadBalancer

---
+++ k8s_cheatSheet.sh
@@ -0,0 +1,10 @@
+
+source <(kubectl completion bash) # setup autocomplete in bash into the current shell, bash-completion package should be installed first.
+echo "source <(kubectl completion bash)" >> ~/.bashrc # add autocomplete permanently to your bash shell.
+
+alias k=kubectl
+complete -F __start_kubectl k
+
+echo;
+echo "https://kubernetes.io/docs/reference/kubectl/cheatsheet/#kubectl-autocomplete"
+echo;
