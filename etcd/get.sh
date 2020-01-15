e0b3393054da098f2083f913677487c64d85dc93
rolroralra <rolroralra@gmail.com>
2020-01-15 10:23:15 +0900

    etcd install

---
+++ etcd/get.sh
@@ -0,0 +1,1 @@
+sudo ETCDCTL_API=3 ./etcdctl --endpoints 127.0.0.1:2379 --cacert /etc/kubernetes/pki/etcd/ca.crt --cert /etc/kubernetes/pki/etcd/server.crt --key /etc/kubernetes/pki/etcd/server.key get
